c69e84748a8058234241c14feeadeab22d6c2f0f
Nicola Bitetto <nicolaandresr@gmail.com>
2020-02-17 12:23:24 -0400

    Week 2 / Filters

--- module 1-Coding-Assignment-Solution/app.js
+++ module 1-Coding-Assignment-Solution/app.js
@@ -1,32 +1,32 @@
-(function () {
-   'use strict';
+(function() {
+  "use strict";
 
-   angular.module('LunchCheck', [])
-      .controller('LunchCheckController', LunchCheckController);
+  angular
+    .module("LunchCheck", [])
+    .controller("LunchCheckController", LunchCheckController);
 
-   LunchCheckController.$inject = ['$scope'];
+  LunchCheckController.$inject = ["$scope"];
 
-   function LunchCheckController($scope) {
-      $scope.answer = "";
+  function LunchCheckController($scope) {
+    $scope.answer = "";
 
-      $scope.checkDishes = function () {
-         var items;
+    $scope.checkDishes = function() {
+      var items;
 
-         if ($scope.input) {
-
-            items = $scope.input.split(",");
-            items = items.filter(function (item) {
-               return item.length > 0;
-            });
-            if (items.length <= 3) {
-               $scope.answer = "Enjoy!";
-               $scope.textColor = "Green";
-            } else {
-               $scope.answer = "Too much!";
-            }
-         } else {
-            $scope.answer = "Please enter data first";
-         }
-      };
-   }
+      if ($scope.input) {
+        items = $scope.input.split(",");
+        items = items.filter(function(item) {
+          return item.length > 0;
+        });
+        if (items.length <= 3) {
+          $scope.answer = "Enjoy!";
+          $scope.textColor = "Green";
+        } else {
+          $scope.answer = "Too much!";
+        }
+      } else {
+        $scope.answer = "Please enter data first";
+      }
+    };
+  }
 })();
